99d0619ded7cba188a9a9d60961f58e1d3ea9c0a
ompatil <op15222612@eng.rizvi.edu.in>
2025-10-06 16:13:59 +0530

    experiment1a.c

---
+++ experiment1a.c
@@ -0,0 +1,13 @@
+Start here
+program_1.c X
+1234567891011122 13
+#include<stdio.h>
+int main()
+day
+printf("hello world\n");
+printf("NAME: om dipak patil\n"):
+printf(" AGE 17\n");
+printf(" CLASS:civil\n");
+printf(" DIVISION: B\n");
+printf("UIN:2510014\n");
+return 0;
