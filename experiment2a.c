a7d563041e67f7af15398e63875aadccf2bbdef5
ompatil <op15222612@eng.rizvi.edu.in>
2025-10-08 17:02:55 +0530

    experiment2a.c

---
+++ experiment2a.c
@@ -0,0 +1,18 @@
+/* Name: Om
+UIN: 2510014
+Field: Civil
+Div : B*/
+#include <stdio.h>
+int main()
+{
+int whether;
+printf("enter a number: ");
+scanf("%d", &num);
+if (num %2==0){
+printf("%d is an even number.\n", num);
+}else {
+printf ("%d is an odd number.\n",num);
+}
+return 0;
+
+}
