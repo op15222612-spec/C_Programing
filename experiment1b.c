3077dccf634b92c732945161c1870b8fa58f36e6
ompatil <op15222612@eng.rizvi.edu.in>
2025-10-08 16:59:06 +0530

    experiment1b.c

--- experiment1b.c
+++ experiment1b.c
@@ -1,52 +1,21 @@
-1 #include<stdio.h>
-2
-3 int main()
-4{
-5
-int physics;
-6
-printf("enter your marks");
-7
-scanf("%d", &physics);
-8
-9
-int math;
-10
-printf("enter your marks");
-11
-scanf("%d",&math);
-12
-13
-int chemistry;
-14
-printf("enter your marks");
-15
-scanf("%d", &chemistry);
-16
-int sum;
-17
-18
-float avg;
-19
-20
-sum = physics math + chemistry;
-avg (float)sum/3;
-21
-printf("sum is %d \n avg is %f \n", sum, avg);
-22
-23
-1f (avg >= 50) {
-24
-printf("\n you are eligible to admission.");
-25
-else {
-26
-printf("\n you are not eligible to admission.");
-27
-}
-28
-29
-30
+/* Name: Om Dipak Patil
+UIN: 2510014
+FIELD: CIVIL
+DIV: B*/
+#include <stdio.h>
+int main() {
+float physics, chemistry, math, average;
+printf("Enter marks in Physics: ");
+scanf("%f", &physics);
+printf("Enter marks in Chemistry: ");
+scanf("%f", &chemistry);
+printf("Enter marks in Mathematics: ");
+scanf("%f", &math);
+average = (physics + chemistry+ math) / 3;
+printf("\nAverage Marks = %.2f\n", average);
+(average >= 50)
+? printf("Eligible for admission.\n")
+: printf("Not eligible for admission.\n");
 return 0;
-31
-32}
+
+}
