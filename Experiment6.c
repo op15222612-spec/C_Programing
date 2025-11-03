8afe10eae79f356919c3a67fc448825f8d9525ab
ompatil <op15222612@eng.rizvi.edu.in>
2025-11-03 08:07:35 +0530

    Experiment6.c

---
+++ Experiment6.c
@@ -0,0 +1,14 @@
+/*Name :Om Dipak Patil 
+Field:Civil
+UIN:251C014*/
+#include <stdio.h>
+int storage_class()
+{
+   int abc = 0;
+   printf("Counter is %d \n ",abc);
+}
+ int main()
+{
+   storage_class();
+   return 0;
+}
