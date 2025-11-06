ae6d28a1c9ac31387110f658c1e3d677d7a6b84b
ompatil <op15222612@eng.rizvi.edu.in>
2025-11-06 08:02:01 +0530

    Experiment8.c
    Fix syntax errors and improve palindrome check logic.

---
+++ Experiment8.c
@@ -0,0 +1,39 @@
+/*Name: Om Dipak Patil
+
+Branch: Civil
+
+Uin:251C014
+
+Roll:11*/
+
+#include <stdio.h>
+
+int main() {
+
+char str[100];
+
+int i, length, flag-;
+
+printf("Enter a string: ");
+
+gets (str);
+
+// Find string Length
+
+for (i=0; str[i] != '\0'; i++) {
+
+}
+
+length;
+
+// Check palindrome
+
+for (10; i < length / 2; i++) {
+
+if (str[i] = str[length-i-1]) {
+
+flag = 1;
+
+break;
+
+}
