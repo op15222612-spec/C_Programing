b9e86016a9a9cc3901c53dec00a86d66a9a72591
ompatil <op15222612@eng.rizvi.edu.in>
2025-11-06 07:57:04 +0530

    experiment7a.c
    Fix syntax errors and improve input handling.

---
+++ experiment7a.c
@@ -0,0 +1,29 @@
+/name:om dipak patil
+
+feild:civil
+
+uin; 2510014
+
+#include <stdio.h>
+
+int main(){
+
+int n, i, arr[100], largest;
+
+print("enter number of elements: ");
+
+scam("%d", &n);
+
+print("enter %d elements: \n", n); for (i=0; i<n; i++){ }
+
+scanf("%d",&arr[i]);
+
+largest arr[0];
+
+for (i=1; i<n; i++) { if (arr[i]> largest); }
+
+printi ("Largest element = %d\n", largest);
+
+return 0;
+
+}
