c30763fbbd7a5ddfa4e2e7b2c8d776780ce7e64f
ompatil <op15222612@eng.rizvi.edu.in>
2025-11-10 07:44:17 +0530

    Experiment5b.c
    Implement factorial function and main to print result.

---
+++ experiment5b.c
@@ -0,0 +1,21 @@
+* name: Om Dipak Patil
+uin : 2510014
+branch: civil*/
+#include <stdio.h>
+int factorial(int num)
+{
+if (num num 1)
+{
+return 1;
+}
+else
+}
+return num factorial(num 1);
+}
+}
+int main()
+{
+printf("Factorial of 5 is %d\n", factorial(5));
+return 0;
+
+}
