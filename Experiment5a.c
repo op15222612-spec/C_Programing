f1f77a9418eb8fd52af074a62c4f3f401509f4f6
ompatil <op15222612@eng.rizvi.edu.in>
2025-11-10 07:40:47 +0530

    Experiment5a.c

---
+++ Experiment5a.c
@@ -0,0 +1,19 @@
+/* name: Om Dipak Patil
+uin: 2510014
+branch: civil*/
+#include <stdio.h>
+int factorial(int num)
+}
+int fact = 1;
+for (int i= num; i >= 1; i--)
+{
+fact fact * i;
+}
+return fact;
+}
+int main()
+{
+int xyz factorial(5);
+printf("Factorial of 5 is %d\n", xyz);
+return 0;
+}
